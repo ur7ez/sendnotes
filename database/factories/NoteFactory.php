34d09a7f344e41d97866bd0e48d71aecc7fcd46b
ur7ez <ur7ez@ukr.net>
2024-05-16 18:50:08 +0300

    adjust notes seeder

--- database/factories/NoteFactory.php
+++ database/factories/NoteFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,11 +19,14 @@ class NoteFactory extends Factory
     public function definition(): array
     {
         return [
+            'id' => fake()->uuid,
+            'user_id' => User::factory(),
             'title' => fake()->sentence,
-            'body' => fake()->sentence,
-            'send_date' => now(),
-            'is_published' => false,
-            'heart_count' => 0,
+            'body' => fake()->paragraph,
+            'recipient' => fake()->email,
+            'send_date' => fake()->dateTimeBetween('now', '+15 days'),
+            'is_published' => true,
+            'heart_count' => fake()->numberBetween(0, 20),
         ];
     }
 }
